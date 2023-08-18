43dbd838a6d8cec0e87e5dfcff7ee0098c22d4b6
Bricio Gonzalo Vellaz Barbieri <briciovellaz@gmail.com>
2023-08-18 11:41:03 -0300

    Add app theme as a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bricio_vellaz/home.dart';
 import 'package:flutter/material.dart';
+import 'movie_app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,51 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: const ColorScheme(
-          primary: Colors.red,
-          secondary: Colors.white70,
-          tertiary: Colors.white70,
-          brightness: Brightness.light,
-          onPrimary: Colors.white,
-          onSecondary: Colors.white,
-          error: Colors.red,
-          onError: Colors.black,
-          onBackground: Colors.white,
-          background: Colors.black87,
-          surface: Colors.black87,
-          onSurface: Colors.white70,
-        ),
-        brightness: Brightness.light,
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.white,
-            fontSize: 25,
-          ),
-          titleMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-          titleSmall: TextStyle(
-            color: Colors.white,
-            fontSize: 17,
-          ),
-          bodyMedium: TextStyle(
-            color: Colors.white70,
-            fontSize: 17,
-          ),
-          bodySmall: TextStyle(
-            color: Colors.white70,
-            fontSize: 15,
-          ),
-        ),
-        primaryColor: Colors.white,
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.red,
-          iconSize: 30,
-        ),
-        //theme for the entire app
-      ),
+      theme: movieAppData(),
       home: const Scaffold(
         body: SafeArea(
           child: Home(),
---
+++ lib/movie_app_theme.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+ThemeData movieAppData() {
+  const Color mainColor=Colors.white;
+  const Color secondaryColor=Colors.white70;
+  return ThemeData(
+    colorScheme: const ColorScheme(
+      primary: Colors.red,
+      secondary: secondaryColor,
+      tertiary: secondaryColor,
+      brightness: Brightness.light,
+      onPrimary: mainColor,
+      onSecondary: mainColor,
+      error: Colors.red,
+      onError: Colors.black,
+      onBackground: mainColor,
+      background: Colors.black87,
+      surface: Colors.black87,
+      onSurface: secondaryColor,
+    ),
+    brightness: Brightness.light,
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        color: mainColor,
+        fontSize: 25,
+      ),
+      titleMedium: TextStyle(
+        color: secondaryColor,
+        fontSize: 20,
+      ),
+      titleSmall: TextStyle(
+        color: secondaryColor,
+        fontSize: 17,
+      ),
+      bodyMedium: TextStyle(
+        color: secondaryColor,
+        fontSize: 17,
+      ),
+      bodySmall: TextStyle(
+        color: secondaryColor,
+        fontSize: 15,
+      ),
+    ),
+    primaryColor: mainColor,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.red,
+      iconSize: 30,
+    ),
+    //theme for the entire app
+  );
+}

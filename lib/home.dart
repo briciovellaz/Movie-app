bde27382eef3dbdcf1622ad071721fb63e1d2598
Bricio Gonzalo Vellaz Barbieri <briciovellaz@gmail.com>
2023-08-18 11:43:13 -0300

    Add factory method to movie

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,6 @@
 import 'package:bricio_vellaz/movie.dart';
 import 'package:bricio_vellaz/movie_card.dart';
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,18 +8,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MovieCard(
-      movie: Movie(
-          1,
-          'The Super Mario Bros. Movie',
-          'The Super Mario Bros. Movie',
-          ['Animation', 'Family', 'Adventure', 'Fantasy', 'Comedy'],
-          'mario_poster',
-          'mario_backdrop',
-          '2023-04-05',
-          '7,8',
-          'While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi '
-              'are transported down a mysterious pipe and wander into a magical new world. But when the '
-              'brothers are separated, Mario embarks on an epic quest to find Luigi.'),
+      movie: Movie.marioExample(),
     );
   }
 }
--- lib/movie.dart
+++ lib/movie.dart
@@ -20,4 +20,20 @@ class Movie {
     this.voteAverage,
     this.overview,
   );
+
+  factory Movie.marioExample() {
+    return Movie(
+      1,
+      'The Super Mario Bros. Movie',
+      'The Super Mario Bros. Movie',
+      ['Animation', 'Family', 'Adventure', 'Fantasy', 'Comedy'],
+      'mario_poster',
+      'mario_backdrop',
+      '2023-04-05',
+      '7,8',
+      'While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi '
+          'are transported down a mysterious pipe and wander into a magical new world. But when the '
+          'brothers are separated, Mario embarks on an epic quest to find Luigi.',
+    );
+  }
 }
